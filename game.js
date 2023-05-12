deb85eaff78314cd257dee4e6ff15e71d5ca89af
fariazz <fariazz@gmail.com>
2023-05-12 18:17:51 +1000

    level backgrounds, sound

--- game.js
+++ game.js
@@ -8,7 +8,14 @@ const config = {
     canvas: document.getElementById('gameCanvas'),
     type: Phaser.CANVAS,
     scene: [MainScene],
-    pixelArt: true
+    pixelArt: true,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'gameContainer',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
 };
 
 const game = new Phaser.Game(config);
--- mainScene.js
+++ mainScene.js
@@ -14,7 +14,19 @@ export class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        // Add background image preload here
+        this.load.image('digger', 'digger.png');
+        this.load.image('background0', 'background0.jpg');
+        this.load.image('background1', 'background1.jpg');
+        this.load.image('background2', 'background2.jpg');
+        this.load.image('background3', 'background3.jpg');
+        this.load.image('background4', 'background4.jpg');
+        this.load.image('background5', 'background5.jpg');
+        this.load.image('background6', 'background6.jpg');
+        this.load.image('background7', 'background7.jpg');
+        this.load.image('background8', 'background8.jpg');
+        this.load.image('background9', 'background9.jpg');
+        this.load.audio('diggerSound', 'digger.wav');
+        this.load.audio('winSound', 'win.mp3');
     }
 
     create() {
@@ -22,16 +34,77 @@ export class MainScene extends Phaser.Scene {
         this.setUpTracingMechanism();
 
         // Add background image creation here
+        this.backgroundImage = this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background1'); // Replace 'background' with the key of your background image
+        this.backgroundImage.setOrigin(0.5, 0.5); // Set the origin to the top-left corner (optional)
+        this.backgroundImage.setScale(0.8); // Adjust the scale of the background image (optional)
+        this.backgroundImage.setDepth(-1);
 
         // Add sprite to be shown when the user traces the number here
+        this.cursorSprite = this.add.sprite(0, 0, 'digger'); // Replace 'spriteKey' with your actual sprite key
+        this.cursorSprite.setScale(0.1);
+        this.cursorSprite.setVisible(false); // Initially hide the sprite
+        this.cursorSprite.setDepth(1);
+
+        this.cursorSound = this.sound.add('diggerSound', {
+            loop: true
+        });
+
+        this.winSound = this.sound.add('winSound');
 
         this.currentNumber = this.pickRandomNumber();
         this.drawNumber(this.currentNumber);
+
+        // Create a restart button
+        const restartButton = this.add.text(20, 10, 'RESTART', {
+            font: '20px Arial',
+            fill: '#fff',
+            stroke: '#fff',
+            align: 'left'
+        });
+        restartButton.setDepth(2)
+        restartButton.setInteractive(); // Enable interactivity for the button
+
+        // Add a click event listener to the button
+        restartButton.on('pointerdown', () => {
+            this.restartGame();
+        });
+
+        const textBackground = this.add.graphics();
+        textBackground.fillStyle(0xc55add, 1); // Set the background color and transparency
+        textBackground.fillRect(
+            restartButton.x - 10, // Adjust the padding around the text
+            restartButton.y - 5,
+            restartButton.width + 20,
+            restartButton.height + 10
+        );
+
+    }
+
+    restartGame() {
+        // Custom logic to restart the game
+        // Reset game state, positions, scores, etc.
+        // ...
+
+        // Restart the scene or game
+        this.scene.restart(); // Use `this.scene.start(sceneKey)` if you want to start a specific scene
     }
 
     createNumberSprite() {
         const canvasCenterX = this.game.config.width / 2;
         const canvasCenterY = this.game.config.height / 2;
+
+        const background = this.add.graphics();
+        background.fillStyle(0x000000, 0.4);
+        const width = 280;
+        const height = 400;
+
+        background.fillRect(
+            canvasCenterX - width / 2,
+            canvasCenterY - height / 2,
+            width,
+            height
+        );
+
         this.numberSprite = this.add.text(canvasCenterX, canvasCenterY, '', {
             font: '400px Arial',
             fill: '#808080',
@@ -44,16 +117,11 @@ export class MainScene extends Phaser.Scene {
     }
 
     drawNumber(number) {
-        this.numberSprite.setText(number.toString());
+        let newNumber = number.toString();
+        this.numberSprite.setText(newNumber);
+        this.backgroundImage.setTexture(`background${newNumber}`)
 
-        // Wait for the next frame to be drawn before updating the image data and gray pixel count
-        // requestAnimationFrame(() => {
-        //     this.originalImageData = getImageData(this.game.canvas);
-        //     this.originalGrayPixels = countGrayPixels(this.originalImageData);
-        //     console.log(this.originalGrayPixels)
-        //     console.log(this.originalImageData)
-        // });
-        this.time.delayedCall(100, () => {
+        this.time.delayedCall(50, () => {
             this.originalImageData = getImageData(this.game.canvas, this.numberSprite);
             this.originalGrayPixels = countGrayPixels(this.originalImageData);
         });
@@ -69,41 +137,70 @@ export class MainScene extends Phaser.Scene {
 
         console.log(tracedPercentage, extraTracingPercentage)
 
-        const tracingThreshold = 0.7;
+        const tracingThreshold = 0.8;
         const extraTracingThreshold = 2;
 
         return tracedPercentage > tracingThreshold && extraTracingPercentage < extraTracingThreshold;
     }
 
     pickRandomNumber() {
-        return Math.floor(Math.random() * 10) + 1;
+        return Math.floor(Math.random() * 9) + 0;
     }
 
     setUpTracingMechanism() {
         this.numberSprite.on('pointerdown', (pointer) => {
+            if (this.isWinning) return;
+
             this.isDrawing = true;
-            this.graphics = this.add.graphics();
+
+            this.cursorSprite.setVisible(true);
+            this.cursorSound.play();
+
+            if (!this.graphics)
+                this.graphics = this.add.graphics();
+
             this.graphics.lineStyle(50, 0x0000ff, 1);
             this.graphics.beginPath();
             this.graphics.moveTo(pointer.x, pointer.y); // Move to the starting position
         });
 
         this.numberSprite.on('pointermove', (pointer) => {
+            if (this.isWinning) return;
             if (!this.isDrawing) return;
             this.graphics.lineTo(pointer.x, pointer.y);
             this.graphics.strokePath();
+
+            if (this.input.activePointer.isDown) {
+                this.cursorSprite.setPosition(pointer.x, pointer.y); // Update the sprite position based on the pointer position
+            }
         });
 
         this.numberSprite.on('pointerup', () => {
+            if (this.isWinning) return;
+
             this.isDrawing = false;
+            
+
+            this.cursorSprite.setVisible(false);
+            this.cursorSound.pause();
+
             // Check if the user traced the number correctly
             if (this.checkTracing()) {
-                alert('Good job!');
-                this.currentNumber = this.pickRandomNumber();
-                this.drawNumber(this.currentNumber);
+                this.isWinning = true;
+                this.winSound.play();
+
+                this.winSound.once('complete', () => {
+                    this.isWinning = false;
+                    this.graphics.destroy();
+                    this.graphics = null;
+
+                    this.currentNumber = this.pickRandomNumber();
+                    this.drawNumber(this.currentNumber);
+                });
+
+
             }
-            this.graphics.destroy(); // Destroy the graphics object
-            this.graphics = null; // Set the graphics object to null to free up memory
+
         });
     }
 
